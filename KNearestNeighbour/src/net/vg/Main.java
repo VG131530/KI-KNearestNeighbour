f2a6fb5a35c7e841157a07f4bf9c811e186eec0d
vg-131530 <vg-131530@hs-weingarten.de>
2017-01-16 18:32:00 +0100

    added File.Separator

--- KNearestNeighbour/src/net/vg/Main.java
+++ KNearestNeighbour/src/net/vg/Main.java
@@ -1,14 +1,16 @@
 package net.vg;
 
+import java.io.File;
+
 public class Main {
 
 	public static void main(String[] args) {
 
 		int NEIGHBOUR_COUNT = 3;
 		
-		double[][] testData = DataFileReader.readFile("DataFolder\\app1.test");
-		double[][] trainData = DataFileReader.readFile("DataFolder\\app1.data");
-		double[][] testDataWONormation = DataFileReader.readFile("DataFolder\\app1.test");
+		double[][] testData = DataFileReader.readFile("DataFolder" + File.separator +"app1.test");
+		double[][] trainData = DataFileReader.readFile("DataFolder" + File.separator +"app1.data");
+		double[][] testDataWONormation = DataFileReader.readFile("DataFolder" + File.separator +"app1.test");
 
 		
 		new KNearesNeighbourHeuristic(NEIGHBOUR_COUNT, testData, trainData, testDataWONormation).doMagic();
